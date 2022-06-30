07da71c9a97347398b5c2f4c533cda170260730a
patriciarojasp <patricia.rojasperez@hashicorp.com>
2022-06-30 11:43:12 -0500

    added terraform providers block to azure

--- assets/azure/main.tf
+++ assets/azure/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   subscription_id = "<subscription_id>"
