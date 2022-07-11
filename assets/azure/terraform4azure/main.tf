b56f14a5e944e60b7fbdbaeca5ae2ee28c1bf200
patriciarojasp <patricia.rojasperez@hashicorp.com>
2022-07-11 17:07:45 -0500

    added variables.tf & changed corresponding vars in main.tf

--- assets/azure/terraform4azure/main.tf
+++ assets/azure/terraform4azure/main.tf
@@ -14,13 +14,12 @@ terraform {
   }
 }
 
-
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
-  subscription_id = "<subscription_id>"
-  client_id       = "<client_id>"
-  client_secret   = "<client_secret>"
-  tenant_id       = "<tenant_id>"
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
 
   features {}
 }
@@ -41,23 +40,47 @@ data "azurerm_image" "main" {
 }
 
 output "image_id" {
-  value = "/subscriptions/<subscription_id>/resourceGroups/RG-EASTUS-SPT-PLATFORM/providers/Microsoft.Compute/images/myPackerImage"
+  value = "/subscriptions/${var.subscription_id}/resourceGroups/RG-EASTUS-SPT-PLATFORM/providers/Microsoft.Compute/images/myPackerImage"
 }
 
 # Create a Network Security Group with some rules
 resource "azurerm_network_security_group" "main" {
-  name                = "my-SG"
-  location            = data.azurerm_resource_group.main.location
+  name                = "${var.prefix}-sg"
+  location            = var.location
   resource_group_name = data.azurerm_resource_group.main.name
 
   security_rule {
-    name                       = "my-SGR"
+    name                       = "HTTP"
     priority                   = 100
     direction                  = "Inbound"
     access                     = "Allow"
     protocol                   = "Tcp"
-    source_port_range          = "80"
-    destination_port_range     = "*"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "HTTPS"
+    priority                   = 102
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 101
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
@@ -66,41 +89,37 @@ resource "azurerm_network_security_group" "main" {
 
 # Create virtual network
 resource "azurerm_virtual_network" "main" {
-  name                = "my-network"
-  address_space       = ["10.0.0.0/16"]
-  location            = data.azurerm_resource_group.main.location
+  name                = "${var.prefix}-vnet"
+  address_space       = [var.address_space]
+  location            = var.location
   resource_group_name = data.azurerm_resource_group.main.name
 }
 
 # Create subnet
 resource "azurerm_subnet" "main" {
-  name                 = "my-subnet"
+  name                 = "${var.prefix}-subnet"
   resource_group_name  = data.azurerm_resource_group.main.name
   virtual_network_name = azurerm_virtual_network.main.name
-  address_prefixes     = ["10.0.2.0/24"]
+  address_prefixes     = [var.subnet_prefix]
 }
 
 # Create public IP
 resource "azurerm_public_ip" "main" {
-  name                = "my-public-ip"
+  name                = "${var.prefix}-ip"
   resource_group_name = data.azurerm_resource_group.main.name
-  location            = data.azurerm_resource_group.main.location
+  location            = var.location
   allocation_method   = "Static"
-
-  tags = {
-    environment = "Production"
-  }
-  domain_name_label   = "path-to-packer-YOUR_NAME"
+  domain_name_label   = "path-to-packer-${var.prefix}"
 }
 
 # Create network interface
 resource "azurerm_network_interface" "main" {
-  name                = "my-nic"
-  location            = data.azurerm_resource_group.main.location
+  name                = "${var.prefix}-nic"
+  location            = var.location
   resource_group_name = data.azurerm_resource_group.main.name
 
   ip_configuration {
-    name                          = "internal"
+    name                          = "${var.prefix}-ipconfig"
     subnet_id                     = azurerm_subnet.main.id
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.main.id
@@ -110,35 +129,34 @@ resource "azurerm_network_interface" "main" {
 # Create a new Virtual Machine based on the custom Image
 resource "azurerm_virtual_machine" "myVM" {
   name                             = "myVM"
-  location                         = data.azurerm_resource_group.main.location
+  location                         = var.location
   resource_group_name              = data.azurerm_resource_group.main.name
   network_interface_ids            = ["${azurerm_network_interface.main.id}"]
-  vm_size                          = "Standard_DS1_V2"
+  vm_size                          = var.vm_size
   delete_os_disk_on_termination    = true
   delete_data_disks_on_termination = true
 
   storage_image_reference {
-    id = "${data.azurerm_image.main.id}"
+    id        = "${data.azurerm_image.main.id}"
   }
 
   storage_os_disk {
-    name              = "myVM2-OS"
+    name              = "${var.prefix}-osdisk"
     caching           = "ReadWrite"
     create_option     = "FromImage"
     managed_disk_type = "Standard_LRS"
 }
 
   os_profile {
-    computer_name  = "APPVM"
-    admin_username = "devopsadmin"
-    admin_password = "Cssladmin#2019"
+    computer_name  = var.prefix
+    admin_username = var.admin_username
+    admin_password = var.admin_password
   }
 
   os_profile_linux_config {
     disable_password_authentication = false
   }
 
-  
   tags = {
     environment = "Production"
   }
@@ -148,10 +166,8 @@ resource "azurerm_virtual_machine" "myVM" {
         destination = "/tmp/index.html"
         connection {
             type     = "ssh"
-            # user     = var.admin_username
-            # password = var.admin_password
-            user     = "devopsadmin"
-            password = "Cssladmin#2019"
+            user     = var.admin_username
+            password = var.admin_password
             host     = azurerm_public_ip.main.fqdn
         }
     }
@@ -167,14 +183,13 @@ resource "azurerm_virtual_machine" "myVM" {
     ]
     connection {
       type     = "ssh"
-      # user     = var.admin_username
-      # password = var.admin_password
-      user     = "devopsadmin"
-      password = "Cssladmin#2019"
+      user     = var.admin_username
+      password = var.admin_password
       host     = azurerm_public_ip.main.fqdn
     }
   }
 }
-output "app_url" {
+
+output "your_app_url" {
     value = "http://${azurerm_public_ip.main.fqdn}"
-  }
+}
---
+++ assets/azure/terraform4azure/variables.tf
@@ -0,0 +1,60 @@
+##############################################################################
+# Variables File
+# 
+# Here is where we store the default values for all the variables used in our
+# Terraform code. If you create a variable with no default, the user will be
+# prompted to enter it (or define it via config file or command line flags.)
+variable "prefix" {
+  description = "This prefix will be included in the name of most resources."
+  default = "<YOUR_NAME>"
+}
+
+variable "subscription_id" {
+  description = "This is the subscription id that the azure cli will provide you with."
+  default = "<SUBSCRIPTION_ID"
+}
+
+variable "client_id" {
+  description = "This is the client id that the azure cli will provide you with."
+  default = "<CLIENT_ID>"
+}
+
+variable "client_secret" {
+  description = "This is the client secret that the azure cli will provide you with."
+  default = "<CLIENT_SECRET>"
+}
+
+variable "tenant_id" {
+  description = "This is the tenant id that the azure cli will provide you with."
+  default = "<TENANT_ID>"
+}
+
+variable "location" {
+  description = "The region where the virtual network is created."
+  default     = "centralus"
+}
+
+variable "address_space" {
+  description = "The address space that is used by the virtual network. You can supply more than one address space. Changing this forces a new resource to be created."
+  default     = "10.0.0.0/16"
+}
+
+variable "subnet_prefix" {
+  description = "The address prefix to use for the subnet."
+  default     = "10.0.10.0/24"
+}
+
+variable "vm_size" {
+  description = "Specifies the size of the virtual machine."
+  default     = "Standard_DS1_V2"
+}
+
+variable "admin_username" {
+  description = "Administrator user name for linux and mysql"
+  default     = "hashicorp"
+}
+
+variable "admin_password" {
+  description = "Administrator password for linux and mysql"
+  default     = "Password123!"
+}
